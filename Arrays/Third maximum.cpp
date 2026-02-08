1f76a8946655eb0bc6ede884b48c7dc06f181c8b
Janani-bn <hemapriyan@Jananis-MacBookAir.local>
2026-02-08 20:57:12 +0530

    third maximum commit

---
+++ Arrays/Third maximum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int thirdMax(vector<int>& nums) {
+    long long first = LLONG_MIN, second = LLONG_MIN, third = LLONG_MIN;
+
+    for (int x : nums) {
+        if (x == first || x == second || x == third)
+            continue;   // skip duplicates
+
+        if (x > first) {
+            third = second;
+            second = first;
+            first = x;
+        }
+        else if (x > second) {
+            third = second;
+            second = x;
+        }
+        else if (x > third) {
+            third = x;
+        }
+    }
+
+    return (third == LLONG_MIN) ? first : third;
+}
+
+int main() {
+    vector<int> nums = {2, 2, 3, 1};
+    cout << thirdMax(nums);
+    return 0;
+}
